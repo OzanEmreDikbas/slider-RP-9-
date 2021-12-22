4d434c87f7067e65e2d88d6651f64eead6df5d5a
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-22 12:22:15 +0300

    3. commit.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState (0);
+
+  useEffect(() =>{
+    const lastIndex = people.lenght - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index < lastIndex) {
+      setIndex(0);
+    } 
+  }, [index,people])
   return (
     <section className='section'>
       <div className="title">
@@ -17,6 +27,15 @@ function App() {
           const {id, image, name, title, quote} = person;
           // more stuff coming up
           let position = 'nextSlide';
+          if (personIndex === index) {
+            position ='activeSlide';
+          }
+          if (personIndex === index-1 || 
+            (index === 0 && personIndex === people.length - 1)
+            ) {
+            position ='lastSlide';
+          }
+
           return (
             <article className={position} key={id}>
               <img src={image} alt={name} className='person-img'/>
@@ -27,10 +46,10 @@ function App() {
             </article>
           );
         })}
-        <button className='prev'>
+        <button className='prev' onClick={() => setIndex(index-1)}>
           <FiChevronLeft />
         </button>
-        <button className='next'>
+        <button className='next' onClick={() => setIndex(index+1)}>
           <FiChevronRight />
         </button>
       </div>
